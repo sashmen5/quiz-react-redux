ae998be75d30f8dfd2465383e4da8891dc3cd27b
sashmen5 <sashmen5@gmail.com>
2018-12-26 15:39:16 +0200

    Basic card created with qustion and answer

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,11 +1,61 @@
 import React, {Component} from 'react';
-import {Layout} from "@codedojo/mdc-react";
+import './index.css'
+import {
+	Layout,
+	Card,
+	CardHeader,
+	CardSection,
+	LinearProgress,
+	Typography,
+	List,
+	ListItem,
+	ListItemGraphic,
+	Radio
+} from "@codedojo/mdc-react";
 
 class App extends Component {
 
 	render() {
 		return (
-			<Layout>Quiz</Layout>
+			<Layout element="main">
+				<Card>
+					<CardHeader
+						title="Test knowledge JavaSript"
+						subtitle="Question 1 of 5">
+					</CardHeader>
+					<LinearProgress value="25"/>
+
+					<CardSection primary>
+						<Typography>Text of question</Typography>
+						<List>
+							<ListItem>
+								<ListItemGraphic>
+									<Radio/>
+								</ListItemGraphic>
+								<Typography>Varius of answers</Typography>
+							</ListItem>
+
+							<ListItem>
+								<ListItemGraphic>
+									<Radio/>
+								</ListItemGraphic>
+								<Typography>Varius of answers</Typography>
+							</ListItem>
+
+							<ListItem>
+								<ListItemGraphic>
+									<Radio/>
+								</ListItemGraphic>
+								<Typography>Varius of answers</Typography>
+							</ListItem>
+						</List>
+					</CardSection>
+
+
+
+				</Card>
+
+			</Layout>
 		);
 	}
 }

5a60ac5bd3854a2f6abf85648a3daa72f53afb16
sashmen5 <sashmen5@gmail.com>
2018-12-26 15:26:37 +0200

    Ui library of correct version added

---
+++ src/components/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Layout} from "@codedojo/mdc-react";
+
+class App extends Component {
+
+	render() {
+		return (
+			<Layout>Quiz</Layout>
+		);
+	}
+}
+
+
+export default App;
